nonlin: fix jacobian tests

scipy/optimize/nonlin.py
@@ -569,7 +569,7 @@ def asjacobian(J):
                     return m*v
                 else:
                     raise ValueError("Unknown matrix type")
-            def rsolve(self, v):
+            def rsolve(self, v, tol=0):
                 m = J(self.x)
                 if isinstance(m, np.ndarray):
                     return solve(m.conj().T, v)
@@ -697,7 +697,7 @@ class LowRankMatrix(object):
             return solve(self.collapsed, v)
         return LowRankMatrix._solve(v, self.alpha, self.cs, self.ds)
 
-    def rsolve(self, v, tol):
+    def rsolve(self, v, tol=0):
         """Evaluate w = M^-H v"""
         if self.collapsed is not None:
             return solve(self.collapsed.T.conj(), v)
@@ -895,7 +895,7 @@ class BroydenFirst(GenericBroyden):
     def matvec(self, f):
         return self.Gm.solve(f)
 
-    def rsolve(self, f):
+    def rsolve(self, f, tol=0):
         return self.Gm.rmatvec(f)
 
     def rmatvec(self, f):
@@ -1104,7 +1104,7 @@ class Vackar(GenericBroyden):
     def matvec(self, f):
         return -f * self.d
 
-    def rsolve(self, f):
+    def rsolve(self, f, tol=0):
         return -f / self.d.conj()
 
     def rmatvec(self, f):
@@ -1144,7 +1144,7 @@ class LinearMixing(GenericBroyden):
     def matvec(self, f):
         return -f/self.alpha
 
-    def rsolve(self, f):
+    def rsolve(self, f, tol=0):
         return -f*np.conj(self.alpha)
 
     def rmatvec(self, f):
@@ -1195,7 +1195,7 @@ class ExcitingMixing(GenericBroyden):
     def matvec(self, f):
         return -f/self.beta
 
-    def rsolve(self, f, tol):
+    def rsolve(self, f, tol=0):
         return -f*self.beta.conj()
 
     def rmatvec(self, f):

scipy/optimize/tests/test_nonlin.py
@@ -167,28 +167,28 @@ class TestJacobianDotSolve(object):
                     Gv = jac.solve(v)
                     Gv2 = np.linalg.solve(Jd, v)
                     assert_close(Gv, Gv2, 'solve vs array')
-                if hasattr(jac, 'solveH'):
-                    Gv = jac.solveH(v)
+                if hasattr(jac, 'rsolve'):
+                    Gv = jac.rsolve(v)
                     Gv2 = np.linalg.solve(Jd.T.conj(), v)
-                    assert_close(Gv, Gv2, 'solveH vs array')
-                if hasattr(jac, 'dot'):
-                    Jv = jac.dot(v)
+                    assert_close(Gv, Gv2, 'rsolve vs array')
+                if hasattr(jac, 'matvec'):
+                    Jv = jac.matvec(v)
                     Jv2 = np.dot(Jd, v)
                     assert_close(Jv, Jv2, 'dot vs array')
-                if hasattr(jac, 'dotH'):
-                    Jv = jac.dotH(v)
+                if hasattr(jac, 'rmatvec'):
+                    Jv = jac.rmatvec(v)
                     Jv2 = np.dot(Jd.T.conj(), v)
-                    assert_close(Jv, Jv2, 'dotH vs array')
+                    assert_close(Jv, Jv2, 'rmatvec vs array')
 
-            if hasattr(jac, 'dot') and hasattr(jac, 'solve'):
-                Jv = jac.dot(v)
-                Jv2 = jac.solve(jac.dot(Jv))
+            if hasattr(jac, 'matvec') and hasattr(jac, 'solve'):
+                Jv = jac.matvec(v)
+                Jv2 = jac.solve(jac.matvec(Jv))
                 assert_close(Jv, Jv2, 'dot vs solve')
 
-            if hasattr(jac, 'dotH') and hasattr(jac, 'solveH'):
-                Jv = jac.dotH(v)
-                Jv2 = jac.dotH(jac.solveH(Jv))
-                assert_close(Jv, Jv2, 'dotH vs solveH')
+            if hasattr(jac, 'rmatvec') and hasattr(jac, 'rsolve'):
+                Jv = jac.rmatvec(v)
+                Jv2 = jac.rmatvec(jac.rsolve(Jv))
+                assert_close(Jv, Jv2, 'rmatvec vs rsolve')
 
             x = rand(N)
             jac.update(x, self._func(x))